85fedd81c9ca15f5b48a0ab71b3caf411c7ea571
Jhony279 <lordjhony732@gmail.com>
2026-05-20 17:43:29 -0700

    Creating queueu class

--- main.cpp
+++ main.cpp
@@ -10,6 +10,42 @@ struct Customer {
     Customer(string n, string o) : name(n), order(o), next(nullptr) {}
 };
 
+class CoffeQueue {
+private:
+    Customer* front;
+    Customer* rear;
+
+public:
+    CoffeQueue() : front(nullptr), rear(nullptr) {}
+
+    void enqueue(string name, string order) {
+        Customer* newCustomer = new Customer(name, order);
+
+        if(!rear) {
+            front = rear = newCustomer;
+        } else {
+            rear->next = newCustomer;
+            rear = newCustomer;
+        }
+    }
+
+    void dequeue() {
+        if(!front) {
+            cout << "Queue is empty!" << endl;
+            return;
+        }
+
+        Customer* temp = front;
+        front = front->next;
+
+        if(!front) {
+            rear = nullptr;
+        }
+
+        delete temp;
+    }
+};
+
 int main() {
     return 0;
 }
